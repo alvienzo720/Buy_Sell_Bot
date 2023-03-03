b7f8b887e41014816f353dae683a9916f9c8c375
alvienzo720 <hudson1Dellmac>
2023-03-03 13:31:38 +0300

    Add bot commands and uniswap functions

--- src/routes/api/routes.ts
+++ src/routes/api/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { makeOrder, getClosedPnl, getBalance, getKline, cancleOrderController } from "../../controllers";
-import { makeABuy } from "../../exchange/Uniswap";
+
 
 
 
@@ -13,7 +13,7 @@ router.post('/cancel-order', cancleOrderController)
 router.get('/get-closedPnl', getClosedPnl)
 router.get('/get-kline', getKline)
 router.get('/get-balance', getBalance)
-router.get('/uni_buyOrder', makeABuy)
+
 
 
 export { router as makeOrderRoute }
