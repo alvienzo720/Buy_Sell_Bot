18b781c3fdaef5f2719f539bf8bcd7be64000f62
alvienzo720 <hudson1Dellmac>
2023-03-07 13:26:18 +0300

    Modify fetch data

---
+++ src/exchange/uniswap/decodeData.ts
@@ -0,0 +1,5 @@
+import ethers from 'ethers'
+
+const provider =  new ethers.providers.WebSocketProvider(`wss://mainnet.infura.io/ws/v3/68961d62789e403aa86ee13a2d6406b0`)
+
+
--- src/exchange/uniswap/fetchData.ts
+++ src/exchange/uniswap/fetchData.ts
@@ -1,26 +1,66 @@
-const ethers = require("ethers")
+import { providers, utils } from 'ethers'
+import { ABI } from '../../config/ABI'
 
+const erc20interface = new utils.Interface(ABI)
+const provider = new providers.WebSocketProvider(
+    'wss://mainnet.infura.io/ws/v3/68961d62789e403aa86ee13a2d6406b0'
+)
 
-async function fetchTransactions() {
-    const provider = new ethers.providers.WebSocketProvider(`wss://mainnet.infura.io/ws/v3/68961d62789e403aa86ee13a2d6406b0`)
+const main = () => {
     try {
-        provider.on('pending', async (tx: any) => {
-            const txtInfo = await provider.getTransaction(tx)
-            if (txtInfo) {
-                console.log("Transaction Hash", txtInfo.hash)
-                console.log("From", txtInfo.from)
-                console.log("To", txtInfo.to)
-                console.log("Value", txtInfo.value)
-                console.log("Gas Price", txtInfo.gasPrice)
-                console.log("Time Stamo", txtInfo.timestamp)
-                console.log("--------------------------------------------------------------------------")
+        provider.on('pending', async (txHash: string) => {
+            // console.log("hash ", txHash)
+            const txResponse = await getTx(txHash)
+            // console.log(txResponse)
+            if (txResponse) {
+                const inputData = txResponse.data
+                if (inputData != '0x') {
+                    const txData = decode(txHash, inputData)
+                    if (txData) {
+                        console.log('\n\n ', txData!.name, txHash, new Date())
+                    }
+                }
             } else {
-                console.error(`Transaction info for hash "${tx}" not found`)
+                wait(15000)
+                const txResponse = await getTx(txHash)
+
+                if (txResponse) {
+                    const inputData = txResponse!.data
+                    if (inputData != '0x') {
+                        const txData = decode(txHash, inputData)
+                        if (txData) {
+                            console.log('\n\n ', txData!.name, txHash, new Date())
+                        }
+                    }
+                }
             }
         })
     } catch (error) {
-        console.error("An error occurred:", error)
+        console.log('Error ', error)
+    }
+}
+main()
+
+const getTx = async (txHash: string) => {
+    try {
+        const txResponse = await provider.getTransaction(txHash)
+        return txResponse
+    } catch (error) {
+        console.log('Error getting tx ', txHash, error)
     }
 }
 
-fetchTransactions()
+const decode = (txHash: string, inputData: string) => {
+    try {
+        const txData = erc20interface.parseTransaction({ data: inputData })
+
+        console.log("\n\n\n\n\n\n  Got one ", txData)
+        return txData
+    } catch (error) {
+        // console.log("Error ", txHash, error)
+    }
+}
+
+const wait = async (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
